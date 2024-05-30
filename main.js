cd5d1e1368c61ae86079a07137a32a6d4ff73980
Wendel Davi <149116208+Wendel-Costa@users.noreply.github.com>
2024-05-30 19:57:57 -0300

    aula 1

---
+++ main.js
@@ -0,0 +1,12 @@
+const robotron = document.querySelector("#robotron")
+
+robotron.addEventListener("click", function() {
+    dizOi('Fulano');
+})
+
+function dizOi(nome) {
+    alert(`Bem vindo ${nome}!`);
+}
+
+
+
